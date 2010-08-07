8a0bc098417d92796b09a5511732707ea74549ea
Diogo Baeder de Paula Pinto <diogobaeder@yahoo.com.br>
2010-08-07 03:44:10 -0300

    notifications barely working - manually

--- test/notification-init.js
+++ test/notification-init.js
@@ -1,5 +1,9 @@
 var notifier = new Notifier({
-    info: 'information-blue-circle.png',
-    warning: 'exclamation-red-circle.png',
-    ok: 'correct-valid-properties-green-circle.png'
+        info: 'information-blue-circle.png',
+        warning: 'exclamation-red-circle.png',
+        ok: 'correct-valid-properties-green-circle.png'
+    }),
+    $window = $(window);
+$window.click(function(){
+    notifier.checkPermission();
 });
--- test/notification.js
+++ test/notification.js
@@ -22,7 +22,7 @@ var dummyPrototype = {
 var webkitPrototype = {
     checkPermission: function(callback){
         if (this._notifications.checkPermission() !== 0) {
-            this._notifications.requestPermission(callback);
+            this._notifications.requestPermission(callback || noop);
             return false;
         }
         return true;
@@ -32,21 +32,31 @@ var webkitPrototype = {
         if (
             !this.checkPermission(function(){that.info(title, body)})
             ) return;
-        this._notifications.createNotification(this.icons.info, title, body);
+        this._notifications.createNotification(this.icons.info, title, body).show();
+        return true;
     },
     warning: function(title, body){
         var that = this;
         if (
             !this.checkPermission(function(){that.warning(title, body)})
             ) return;
-        this._notifications.createNotification(this.icons.warning, title, body);
+        this._notifications.createNotification(this.icons.warning, title, body).show();
+        return true;
     },
     ok: function(title, body){
         var that = this;
         if (
             !this.checkPermission(function(){that.ok(title, body)})
             ) return;
-        this._notifications.createNotification(this.icons.ok, title, body);
+        this._notifications.createNotification(this.icons.ok, title, body).show();
+        return true;
+    },
+    run: function(icon, title, body, callback) {
+        if (
+            !this.checkPermission(function(){callback(title, body)})
+            ) return false;
+        this._notifications.createNotification(icon, title, body).show();
+        return true;
     }
     
 };
